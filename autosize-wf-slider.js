82205ff120dd181c01436b9c9eea84672ec65161
Tyler Grow <tylergrow@tylergrow.lan>
2021-04-02 03:16:08 -0600

    refactored

--- autosize-wf-slider.js
+++ autosize-wf-slider.js
@@ -1,94 +1,67 @@
-var wfUseAutoSlideHeight = function () {
-    var allSliders = $( '.w-slider' );
-    $( '.w-slider.auto-size' ).attr( 'data-auto-size', true );
-    $( '.w-slider.auto-size' ).css( { opacity: 0 } );
-    var hasSliders = allSliders && allSliders.length && allSliders.length > 0;
-    if ( !hasSliders ) {
-        return;
-    }
-    var sliderMaskDiff = 1;
-    var startSlide = $( '.w-slider.auto-size .w-slide:not([aria-hidden="true"])' );
-    var activeSlideHeight = startSlide.children().height();
-    runSlide( startSlide, activeSlideHeight, autoSliderHeight );
-    function autoSliderHeight () {
-        var els = $( '.w-slider.auto-size .w-slide' );
-        els.each( function ( elIndex ) {
-            var el = $( this );
-            var id = el.attr( 'id' ) || false;
-            if ( !id ) {
-                el.attr( 'id', 'auto-id-slide-' + elIndex );
-                el = $( '#auto-id-slide-' + elIndex );
-                id = el.attr( 'id' );
+
+var activeSlideHeight = 1;
+$( function () {
+    var els = $( '.w-slider.auto-size .w-slide' );
+
+
+
+    var observer = new MutationObserver( function ( mutations ) {
+        mutations.forEach( function ( mutation ) {
+            var mname = mutation.attributeName;
+            var validName = mname == 'aria-hidden';
+            if ( !validName ) {
+                return;
             }
-            var elemToObserve = this;
 
-            var watchingAttr = 'data-watch-slide';
+            var mel = mutation.target;
+            var $mel = $( mel );
+            var isSlide = $mel.hasClass( 'w-slide' ) || $mel.hasClass( '.w-slide' ) || false;
 
-            var watching = el.attr( watchingAttr ) || false;
+            var melVisible = false;
+            if ( !mel.ariaHidden || mel.ariaHidden == 'undefined' || mel.ariaHidden == null || mel.ariaHidden == false ) {
+                melVisible = true;
+            }
+            melVisible = isSlide && melVisible;
+            if ( !melVisible ) {
+                return;
+            }
+            if ( melVisible ) {
 
-            if ( !watching ) {
+                activeSlideHeight = $mel.children().height();
+                var next = $mel.parent().next();
+                var nextHeight = 1;
+                if ( next.height() ) {
+                    nextHeight += next.height() - 1;
 
-                el.attr( watchingAttr, true );
-                var logged = false;
-                var observer = new MutationObserver( function ( mutations ) {
-                    mutations.forEach( function ( mutation ) {
-                        var mname = mutation.attributeName;
 
-                        var validName = mname == 'aria-hidden';
-                        if ( !validName ) {
-                            return;
-                        }
+                }
+                if ( mel.style.marginTop ) {
+                    nextHeight += mel.style.marginTop;
 
-                        var mel = mutation.target;
-                        var isSlide = mel.classList.contains( 'w-slide' );
-                        var matched = mel.id === id;
+                }
+                if ( mel.style.marginBottom ) {
+                    nextHeight += mel.style.marginBottom;
 
-                        var melVisible = false;
-                        if ( !mel.ariaHidden || mel.ariaHidden == 'undefined' || mel.ariaHidden == null || mel.ariaHidden == false ) {
-                            melVisible = true;
-                        }
-                        melVisible = isSlide && matched && melVisible;
-                        if ( !melVisible ) {
-                            return;
-                        }
-                        if ( melVisible ) {
+                }
+                var sliderHeight = nextHeight + activeSlideHeight;
 
-                            activeSlideHeight = $( mel ).children().height();
-                            runSlide( $( mel ), activeSlideHeight );
-                        }
-                    } );
+                $mel.parent().parent().css( {
+                    opacity: 1,
+                    height: sliderHeight + 'px',
                 } );
-                observer.observe( elemToObserve, { attributes: true } );
-
+                $mel.parent().css( {
+                    opacity: 1,
+                    height: activeSlideHeight + 'px',
+                } );
             }
         } );
-    }
-    function runSlide ( freshSlide, freshHeight, callback ) {
-        var mask = freshSlide.parent();
-        var slider = mask.parent();
-        var sliderHeight = slider.height();
-        var maskHeight = mask.height();
-        sliderMaskDiff = sliderHeight - maskHeight;
-        const newSliderHeight = freshHeight + sliderMaskDiff;
-        freshSlide.parent().css( { opacity: 1, height: freshHeight + 'px' } );
-        freshSlide.parent().parent().css( { opacity: 1, height: newSliderHeight + 'px' } );
-        if ( callback ) {
-            callback();
-        }
-
-    }
-
-    var sliderHeightBount = false;
-    window.addEventListener( 'resize', function () {
-        if ( sliderHeightBount && typeof sliderHeightBount == 'function' ) {
-            sliderHeightBount = false;
-            return autoSliderHeight();
-        }
-        return;
     } );
-    autoSliderHeight();
-
-
-};
-
-wfUseAutoSlideHeight();
+    var observingSlides = false;
+    while ( els && els.length && els.length >= 0 && !observingSlides ) {
+        observingSlides = true;
+        return els.each( function ( elIndex ) {
+            var elemToObserve = this;
+            observer.observe( elemToObserve, { attributes: true } );
+        } );
+    }
+} );
